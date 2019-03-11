204ac23a8c4a9a378b974d3cdf6506a167b03469
Ethan Wessel <ethan.wessel@fluidity.io>
2019-03-10 22:54:46 -0400

    update time

--- test/TestTimeContract.js
+++ test/TestTimeContract.js
@@ -2,19 +2,19 @@ const TimeContract = artifacts.require('./TimeContract');
 
 contract('TimeContract', async (accounts) =>  {
   before('deploy TimeContract', async() => {
-    //Sat Mar  2 19:00:00 EST 2019
-    instance_pass = await TimeContract.new(1551571200);
+    //Fri Feb 15 15:53:00 EST 2019
+    instance_1 = await TimeContract.new(1550263980);
     //Tue Mar 19 20:00:00 EDT 2019
-    instance_fail = await TimeContract.new(1553040000);
+    instance_2 = await TimeContract.new(1553040000);
   });
 
-  it("TimeContract Pass", async() => {
-    val = await instance_pass.timeLockedFunction.call();
+  it("TimeContract Fri Feb 15 15:53:00 EST 2019", async() => {
+    val = await instance_1.timeLockedFunction.call();
     assert.equal(val, true, "return value is not true");
   });
 
-  it("TimeContract Fail", async() => {
-    val = await instance_fail.timeLockedFunction.call();
+  it("TimeContract Tue Mar 19 20:00:00 EDT 2019", async() => {
+    val = await instance_2.timeLockedFunction.call();
     assert.equal(val, true, "return value is not true");
   });
 });
