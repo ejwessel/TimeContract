aa8424e278dbead50b8e2d985a51a28346b65b07
Ethan Wessel <ethan.wessel@fluidity.io>
2019-06-28 16:28:39 -0400

    Remove hooks

--- test/TestTimeContract.js
+++ test/TestTimeContract.js
@@ -11,27 +11,21 @@ contract('TimeContract', async (accounts) =>  {
         instance_2 = await TimeContract.new(Wed_Mar_20_00_00_00_UTC_2019);
     });
 
-    beforeEach(async() => {
-        snapShot = await helper.takeSnapshot();
-        snapshotId = snapShot['result'];
-    });
-    afterEach(async() => {
-        await helper.revertToSnapShot(snapshotId);
-    });
+    describe("examples", async() => {
+      it("Sun Feb 10 00:00:00 UTC 2019 (before current time)", async() => {
+          var output = await instance_1.isNowAfter.call();
+          assert.equal(output, true, "output should have been true");
+      });
 
-    it("Sun Feb 10 00:00:00 UTC 2019 (before current time)", async() => {
-        var output = await instance_1.isNowAfter.call();
-        assert.equal(output, true, "output should have been true");
-    });
+      it("Wed Mar 20 00:00:00 UTC 2019 (after current time)", async() => {
+          var output = await instance_2.isNowAfter.call();
+          assert.equal(output, false, "output should have been false");
+      });
 
-    it("Wed Mar 20 00:00:00 UTC 2019 (after current time)", async() => {
-        var output = await instance_2.isNowAfter.call();
-        assert.equal(output, false, "output should have been false");
-    });
-
-    it("Wed Mar 20 00:00:00 UTC 2019 (after current time)", async() => {
-        await helper.advanceTimeAndBlock(SECONDS_IN_DAY * 100); //advance 100 days
-        var output = await instance_2.isNowAfter.call();
-        assert.equal(output, true, "output should have been true");
-    });
+      it("Wed Mar 20 00:00:00 UTC 2019 (after current time)", async() => {
+          await helper.advanceTimeAndBlock(SECONDS_IN_DAY * 100); //advance 100 days
+          var output = await instance_2.isNowAfter.call();
+          assert.equal(output, true, "output should have been true");
+      });
+    })
 });
